f005ac7703ef2780877c10a041ea4a79fd0590ee
Artem Ogai <artem@ogai.dev>
2022-12-06 19:24:11 +0300

    Add nix-shell

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Rust env flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            go
+            go-tools
+            gotools
+            gopls
+            go-outline
+            gocode
+            gopkgs
+            gocode-gomod
+            godef
+            golint
+          ];
+        };
+      });
+}
