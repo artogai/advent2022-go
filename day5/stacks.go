1fb697b379956e7cd1c42f33ba5e7fd097ad625d
Artem Ogai <artem@ogai.dev>
2022-12-08 15:24:42 +0300

    Solve day5 part2

--- day5/stacks.go
+++ day5/stacks.go
@@ -8,54 +8,41 @@ import (
 	"github.com/samber/lo"
 )
 
-func Execute(filename string) []rune {
-	commands := readCommands(filename)
-	s := stacks{
-		stack[rune]{'J', 'H', 'G', 'M', 'Z', 'N', 'T', 'F'},
-		stack[rune]{'V', 'W', 'J'},
-		stack[rune]{'G', 'V', 'L', 'J', 'B', 'T', 'H'},
-		stack[rune]{'B', 'P', 'J', 'N', 'C', 'D', 'V', 'L'},
-		stack[rune]{'F', 'W', 'S', 'M', 'P', 'R', 'G'},
-		stack[rune]{'G', 'H', 'C', 'F', 'B', 'N', 'V', 'M'},
-		stack[rune]{'D', 'H', 'G', 'M', 'R'},
-		stack[rune]{'H', 'N', 'M', 'V', 'Z', 'D'},
-		stack[rune]{'G', 'N', 'F', 'H'},
-	}
-	s = s.executeCommands(commands)
-	return s.listTop()
-}
+type Stacks []Stack[rune]
+type Command struct{ from, to, count int }
 
-type stacks []stack[rune]
-type command struct{ from, to, count int }
-
-func (s stacks) listTop() []rune {
-	return lo.Map(s, func(st stack[rune], _ int) rune {
-		return st.peek()
+func (s Stacks) ListTop() []rune {
+	return lo.Map(s, func(st Stack[rune], _ int) rune {
+		return st.Peek()
 	})
 }
 
-func (s stacks) executeCommands(cmds []command) stacks {
+func (s Stacks) ExecuteCommandsFromFile(filename string, isNewCrane bool) {
+	cmds := readCommands(filename)
+	s.ExecuteCommands(cmds, isNewCrane)
+}
+
+func (s Stacks) ExecuteCommands(cmds []Command, isNewCrane bool) {
 	for _, cmd := range cmds {
-		s = s.executeCommand(cmd)
+		s.ExecuteCommand(cmd, isNewCrane)
 	}
-	return s
 }
 
-func (s stacks) executeCommand(cmd command) stacks {
-	nextFromStack, values := s[cmd.from-1].popMany(cmd.count)
-	nextToStack := s[cmd.to-1].pushMany(values)
-	s[cmd.from-1] = nextFromStack
-	s[cmd.to-1] = nextToStack
-	return s
+func (s Stacks) ExecuteCommand(cmd Command, isNewCrane bool) {
+	values := s[cmd.from].PopMany(cmd.count)
+	if isNewCrane {
+		values = lo.Reverse(values)
+	}
+	s[cmd.to].PushMany(values)
 }
 
-func readCommands(filename string) []command {
+func readCommands(filename string) []Command {
 	lines := file.ReadLines(filename)
-	commands := lo.Map(lines, func(s string, _ int) command { return parseCommand(s) })
+	commands := lo.Map(lines, func(s string, _ int) Command { return parseCommand(s) })
 	return commands
 }
 
-func parseCommand(s string) command {
+func parseCommand(s string) Command {
 	re := regexp.MustCompile(`[-]?\d[\d,]*[\.]?[\d{2}]*`)
 
 	matches := lo.Map(re.FindAllString(s, -1), func(s string, _ int) int {
@@ -63,39 +50,39 @@ func parseCommand(s string) command {
 		return i
 	})
 
-	return command{matches[1], matches[2], matches[0]}
+	return Command{matches[1] - 1, matches[2] - 1, matches[0]}
 }
 
 // naive stack implementation
-type stack[A any] []A
+type Stack[A any] []A
 
-func (s stack[A]) pop() (stack[A], A) {
-	return s[:len(s)-1], s.peek()
+func (s *Stack[A]) Push(v A) {
+	*s = append(*s, v)
 }
 
-func (s stack[A]) push(v A) stack[A] {
-	return append(s, v)
+func (s *Stack[A]) Pop() A {
+	ds := *s
+	res := ds[len(ds)-1]
+	*s = ds[:len(ds)-1]
+	return res
 }
 
-func (s stack[A]) peek() A {
-	return s[len(s)-1]
+func (s *Stack[A]) Peek() A {
+	res := (*s)[len(*s)-1]
+	return res
 }
 
-func (s stack[A]) popMany(n int) (stack[A], []A) {
-	var currEl A
-	buff := make([]A, 0)
-
-	for i := 0; i < n; i++ {
-		s, currEl = s.pop()
-		buff = append(buff, currEl)
-	}
-
-	return s, buff
-}
-
-func (s stack[A]) pushMany(values []A) stack[A] {
+func (s *Stack[A]) PushMany(values []A) {
 	for _, v := range values {
-		s = s.push(v)
+		s.Push(v)
 	}
-	return s
+}
+
+func (s *Stack[A]) PopMany(n int) []A {
+	buff := make([]A, 0, n)
+	for i := 0; i < n; i++ {
+		buff = append(buff, s.Pop())
+	}
+
+	return buff
 }
--- day5/stacks_test.go
+++ day5/stacks_test.go
@@ -5,10 +5,41 @@ import (
 )
 
 func TestExecute(t *testing.T) {
+	s := generateStacks()
+	s.ExecuteCommandsFromFile("commands.txt", false)
+
 	expected := string("TDCHVHJTG")
-	got := string(Execute("commands.txt"))
+	got := string(s.ListTop())
+
 	t.Logf("Expected=%v Got=%v", expected, got)
 	if expected != got {
 		t.Fail()
+	}
+}
+
+func TestExecuteNewCrane(t *testing.T) {
+	s := generateStacks()
+	s.ExecuteCommandsFromFile("commands.txt", true)
+
+	expectedNew := string("NGCMPJLHV")
+	gotNew := string(s.ListTop())
+
+	t.Logf("Expected=%v Got=%v", expectedNew, gotNew)
+	if expectedNew != gotNew {
+		t.Fail()
+	}
+}
+
+func generateStacks() Stacks {
+	return Stacks{
+		Stack[rune]{'J', 'H', 'G', 'M', 'Z', 'N', 'T', 'F'},
+		Stack[rune]{'V', 'W', 'J'},
+		Stack[rune]{'G', 'V', 'L', 'J', 'B', 'T', 'H'},
+		Stack[rune]{'B', 'P', 'J', 'N', 'C', 'D', 'V', 'L'},
+		Stack[rune]{'F', 'W', 'S', 'M', 'P', 'R', 'G'},
+		Stack[rune]{'G', 'H', 'C', 'F', 'B', 'N', 'V', 'M'},
+		Stack[rune]{'D', 'H', 'G', 'M', 'R'},
+		Stack[rune]{'H', 'N', 'M', 'V', 'Z', 'D'},
+		Stack[rune]{'G', 'N', 'F', 'H'},
 	}
 }
