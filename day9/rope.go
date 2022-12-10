74a9091355803535db653aca1efee6eec0886831
Artem Ogai <artem@ogai.dev>
2022-12-10 18:00:29 +0300

    Solve day9 part2

--- day9/rope.go
+++ day9/rope.go
@@ -7,26 +7,6 @@ import (
 	"strings"
 )
 
-func CountRopeVisits(filename string, fieldSize int) int {
-	dirs := readDirections(filename)
-	field := make([][]bool, fieldSize)
-	for i := range field {
-		field[i] = make([]bool, fieldSize)
-	}
-
-	simulateRope(field, dirs)
-
-	cnt := 0
-	for i := range field {
-		for j := range field[i] {
-			if field[i][j] {
-				cnt += 1
-			}
-		}
-	}
-	return cnt
-}
-
 type direction int
 
 const (
@@ -36,41 +16,212 @@ const (
 	Right           = iota
 )
 
-func simulateRope(field [][]bool, dirs []direction) {
-	headX := len(field) / 2
-	headY := headX
-	tailX := headX
-	tailY := headX
+type knot struct{ x, y int }
+type rope struct{ knots []knot }
 
-	for _, dir := range dirs {
-		switch dir {
-		case Up:
-			if tailX > headX {
-				tailX = headX
-				tailY = headY
-			}
-			headX -= 1
-		case Down:
-			if tailX < headX {
-				tailX = headX
-				tailY = headY
-			}
-			headX += 1
-		case Left:
-			if tailY > headY {
-				tailX = headX
-				tailY = headY
-			}
-			headY -= 1
-		case Right:
-			if tailY < headY {
-				tailX = headX
-				tailY = headY
-			}
-			headY += 1
-		}
-		field[tailX][tailY] = true
+func newRope(x, y int, len int) rope {
+	knots := make([]knot, 0, len)
+	for i := 0; i < len; i++ {
+		knots = append(knots, knot{x, y})
 	}
+	return rope{knots}
+}
+
+func (r rope) traverse(dir direction, visited [][]bool, screen [][]rune) {
+	if screen != nil {
+		r.clear(screen)
+	}
+	r.move(dir)
+	r.drawTail(visited)
+	if screen != nil {
+		r.draw(screen)
+	}
+}
+
+func (r rope) move(dir direction) {
+	prevLead := r.knots[0]
+	switch dir {
+	case Up:
+		r.knots[0].x -= 1
+	case Down:
+		r.knots[0].x += 1
+	case Left:
+		r.knots[0].y -= 1
+	case Right:
+		r.knots[0].y += 1
+	}
+
+	for i := 1; i < len(r.knots); i++ {
+		lead := r.knots[i-1]
+		temp := r.knots[i]
+		r.knots[i].move(prevLead, lead)
+		prevLead = temp
+	}
+}
+
+func (k *knot) move(prevLead knot, lead knot) {
+	/*
+		H - lead
+		M - lead, special case
+		O - prev lead
+		T - tail
+	*/
+	if k.x == lead.x-2 || k.x == lead.x+2 ||
+		k.y == lead.y+2 || k.y == lead.y-2 {
+
+		if k.x == prevLead.x && k.y == prevLead.y-1 {
+			/*
+				......
+				....M.
+				..TO..
+				....M.
+				......
+			*/
+			if k.x == lead.x+1 && k.y == lead.y-2 {
+				k.x -= 1
+				k.y += 1
+			} else if k.x == lead.x-1 && k.y == lead.y-2 {
+				k.x += 1
+				k.y += 1
+			} else {
+				k.x = prevLead.x
+				k.y = prevLead.y
+			}
+		} else if k.x == prevLead.x-1 && k.y == prevLead.y {
+			/*
+				......
+				......
+				..T...
+				..O...
+				.M.M..
+			*/
+			if k.x == lead.x-2 && k.y == lead.y+1 {
+				k.x += 1
+				k.y -= 1
+			} else if k.x == lead.x-2 && k.y == lead.y-1 {
+				k.x += 1
+				k.y += 1
+			} else {
+				k.x = prevLead.x
+				k.y = prevLead.y
+			}
+		} else if k.x == prevLead.x && k.y == prevLead.y+1 {
+			/*
+				......
+				.M....
+				..OT..
+				.M....
+				......
+			*/
+			if k.x == lead.x-1 && k.y == lead.y+2 {
+				k.x += 1
+				k.y -= 1
+			} else if k.x == lead.x+1 && k.y == lead.y+2 {
+				k.x -= 1
+				k.y -= 1
+			} else {
+				k.x = prevLead.x
+				k.y = prevLead.y
+			}
+		} else if k.x == prevLead.x+1 && k.y == prevLead.y {
+			/*
+				..M.M.
+				...O..
+				...T..
+				.,....
+				......
+			*/
+			if k.x == lead.x+2 && k.y == lead.y+1 {
+				k.x -= 1
+				k.y -= 1
+			} else if k.x == lead.x+2 && k.y == lead.y-1 {
+				k.x -= 1
+				k.y += 1
+			} else {
+				k.x = prevLead.x
+				k.y = prevLead.y
+			}
+		} else if k.x == prevLead.x-1 && k.y == prevLead.y+1 {
+			/*
+				.........
+				...MHT...
+				...HOH...
+				...HHM...
+				.........
+			*/
+			if k.x == lead.x && k.y == lead.y+2 {
+				k.y -= 1
+			} else if k.x == lead.x-2 && k.y == lead.y {
+				k.x += 1
+			} else {
+				k.x = prevLead.x
+				k.y = prevLead.y
+			}
+		} else if k.x == prevLead.x+1 && k.y == prevLead.y+1 {
+			/*
+				.........
+				..HHM....
+				..HOH....
+				..MHT....
+				.........
+			*/
+			if k.x == lead.x && k.y == lead.y+2 {
+				k.y -= 1
+			} else if k.x == lead.x+2 && k.y == lead.y {
+				k.x -= 1
+			} else {
+				k.x = prevLead.x
+				k.y = prevLead.y
+			}
+		} else if k.x == prevLead.x+1 && k.y == prevLead.y-1 {
+			/*
+				.........
+				..MHH....
+				..HOH....
+				..THM....
+				.........
+			*/
+			if k.x == lead.x && k.y == lead.y-2 {
+				k.y += 1
+			} else if k.x == lead.x+2 && k.y == lead.y {
+				k.x -= 1
+			} else {
+				k.x = prevLead.x
+				k.y = prevLead.y
+			}
+		} else if k.x == prevLead.x-1 && k.y == prevLead.y-1 {
+			/*
+				.........
+				..THM....
+				..HOH....
+				..MHH....
+				.........
+			*/
+			if k.x == lead.x && k.y == lead.y-2 {
+				k.y += 1
+			} else if k.x == lead.x-2 && k.y == lead.y {
+				k.x += 1
+			} else {
+				k.x = prevLead.x
+				k.y = prevLead.y
+			}
+		} else {
+			k.x = prevLead.x
+			k.y = prevLead.y
+		}
+	}
+}
+
+func (r rope) drawTail(visited [][]bool) {
+	visited[r.tail().x][r.tail().y] = true
+}
+
+func (r rope) tail() knot {
+	return r.knots[r.len()-1]
+}
+
+func (r rope) len() int {
+	return len(r.knots)
 }
 
 func readDirections(filename string) []direction {
@@ -82,7 +233,10 @@ func parseDirections(strs ...string) []direction {
 	for _, s := range strs {
 		arr := strings.Split(s, " ")
 		dir := parseDirection(arr[0])
-		n, _ := strconv.Atoi(arr[1])
+		n, err := strconv.Atoi(arr[1])
+		if err != nil {
+			panic(err)
+		}
 		for i := 0; i < n; i++ {
 			dirs = append(dirs, dir)
 		}
@@ -102,4 +256,32 @@ func parseDirection(s string) direction {
 		return Right
 	}
 	panic(fmt.Sprintf("can't parse direction from %s", s))
+}
+
+const SCREEN_EMPTY_RUNE = '.'
+
+func (r rope) draw(screen [][]rune) {
+	for i, knot := range r.knots {
+		screen[knot.x][knot.y] = rune(strconv.Itoa(i)[0])
+	}
+}
+
+func (r rope) clear(screen [][]rune) {
+	for _, knot := range r.knots {
+		screen[knot.x][knot.y] = SCREEN_EMPTY_RUNE
+	}
+}
+
+func (d direction) toString() string {
+	switch d {
+	case Up:
+		return "UP"
+	case Down:
+		return "DOWN"
+	case Left:
+		return "LEFT"
+	case Right:
+		return "RIGHT"
+	}
+	panic(fmt.Sprintf("unmatched direction %v", d))
 }
--- day9/ropte_test.go
+++ day9/ropte_test.go
@@ -1,11 +1,99 @@
 package day9
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
+	"github.com/samber/lo"
 	"github.com/stretchr/testify/require"
 )
 
-func TestRopeSim(t *testing.T) {
-	require.Equal(t, 6642, CountRopeVisits("rope.txt", 1000))
+func TestSimRope(t *testing.T) {
+	require.Equal(t, 6642, calcRopeTailVisited(t, 2, "rope.txt"))
+}
+
+func TestSimRope10(t *testing.T) {
+	require.Equal(t, 2765, calcRopeTailVisited(t, 10, "rope.txt"))
+}
+
+func calcRopeTailVisited(t *testing.T, ropeSize int, filename string) int {
+	fieldSize := 1000
+	visited := make([][]bool, fieldSize)
+
+	for i := 0; i < fieldSize; i++ {
+		visited[i] = make([]bool, fieldSize)
+	}
+
+	pos := fieldSize / 2
+	dirs := readDirections(filename)
+	rope := newRope(pos, pos, ropeSize)
+
+	for _, dir := range dirs {
+		rope.traverse(dir, visited, nil)
+	}
+
+	return calcVisited(visited)
+}
+
+func drawRope(t *testing.T) {
+	fieldSize := 30
+	visited := make([][]bool, fieldSize)
+	screen := make([][]rune, fieldSize)
+
+	for i := 0; i < fieldSize; i++ {
+		visited[i] = make([]bool, fieldSize)
+		screen[i] = make([]rune, fieldSize)
+		for j := 0; j < fieldSize; j++ {
+			screen[i][j] = SCREEN_EMPTY_RUNE
+		}
+	}
+
+	dirs := readDirections("rope_sample.txt")
+	rope := newRope(fieldSize/2, fieldSize/2, 10)
+
+	for _, dir := range dirs {
+		rope.traverse(dir, visited, screen)
+		fmt.Println(dir.toString())
+		fmt.Println(drawScreen(screen))
+	}
+
+	cnt := calcVisited(visited)
+	fmt.Println(drawVisited(visited))
+	fmt.Println(cnt)
+}
+
+func drawScreen(screen [][]rune) string {
+	var str strings.Builder
+	for _, row := range screen {
+		str.WriteString(string(row) + "\n")
+	}
+	return str.String()
+}
+
+func drawVisited(visited [][]bool) string {
+	var str strings.Builder
+	for _, row := range visited {
+		s := string(lo.Map(row, func(b bool, _ int) rune {
+			if b {
+				return '#'
+			} else {
+				return SCREEN_EMPTY_RUNE
+			}
+		}))
+		str.WriteString(s + "\n")
+	}
+	return str.String()
+}
+
+func calcVisited(visited [][]bool) int {
+	cnt := 0
+	for _, row := range visited {
+		for _, v := range row {
+			if v {
+				cnt += 1
+			}
+		}
+	}
+	return cnt
 }
