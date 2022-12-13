f9d14fc89e68f3557f6eb2ed6ed86a68397e47b2
Artem Ogai <artem@ogai.dev>
2022-12-13 21:31:26 +0300

    Solve day12 part2

--- day12/hills_test.go
+++ day12/hills_test.go
@@ -6,7 +6,28 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestHills(t *testing.T) {
+func TestFindShortestPath(t *testing.T) {
 	start, finish, hs := readHeights("hills.txt")
 	require.Equal(t, 440, findShortestPath(start, finish, hs))
+}
+
+func TestFindShortestPathAnyStart(t *testing.T) {
+	_, finish, hs := readHeights("hills.txt")
+	paths := []int{}
+	for i := 0; i < len(hs); i++ {
+		for j := 0; j < len(hs[0]); j++ {
+			if hs[i][j] == 0 {
+				paths = append(paths, findShortestPath(point{i, j}, finish, hs))
+			}
+		}
+	}
+
+	min := paths[0]
+	for _, p := range paths {
+		if p < min {
+			min = p
+		}
+	}
+
+	require.Equal(t, 439, min)
 }
