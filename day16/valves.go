19941bf3dea3eeb907abf1d6cc433fd91eb09552
Artem Ogai <artem@ogai.dev>
2022-12-19 03:10:14 +0300

    Solve day16 part2

--- day16/valves.go
+++ day16/valves.go
@@ -14,20 +14,13 @@ type rates map[valve]int
 func countMaxRate(eds edges, rts rates) int {
 	maxFlow := 0
 	valves := nonEmptyValves(rts)
-	opened := make([]bool, len(valves))
-	paths := make(map[valve]map[valve]int)
+	getPath := shortestPathMemoized(eds)
 
 	var rec func(valve, int, int, []bool)
 	rec = func(v valve, flow int, time int, opened []bool) {
 		for i, nextV := range valves {
 			if !opened[i] {
-				if _, exists := paths[v]; !exists {
-					paths[v] = make(map[valve]int)
-				}
-				if _, exists := paths[v][nextV]; !exists {
-					paths[v][nextV] = shortestPath(v, nextV, &eds)
-				}
-				nextTime := time - paths[v][nextV] - 1
+				nextTime := time - getPath(v, nextV) - 1
 				if nextTime >= 0 {
 					nextFlow := flow + nextTime*rts[nextV]
 					nextOpened := make([]bool, len(opened))
@@ -42,7 +35,75 @@ func countMaxRate(eds edges, rts rates) int {
 		}
 	}
 
-	rec(0, 0, 30, opened)
+	rec(0, 0, 30, make([]bool, len(valves)))
+	return maxFlow
+}
+
+func countMaxRate2(eds edges, rts rates) int {
+	maxFlow := 0
+	valves := nonEmptyValves(rts)
+	getPath := shortestPathMemoized(eds)
+
+	type pathEnd struct {
+		v1    valve
+		v2    valve
+		time1 int
+		time2 int
+	}
+	bestPaths := make(map[pathEnd]int)
+
+	var rec func(pathEnd, int, []bool)
+	rec = func(st pathEnd, flow int, opened []bool) {
+		bestFlow, exists := bestPaths[st]
+		if exists && bestFlow >= flow {
+			return
+		} else {
+			bestPaths[st] = flow
+			bestPaths[pathEnd{st.v2, st.v1, st.time2, st.time1}] = flow
+			for i, nextV1 := range valves {
+				if !opened[i] {
+					for j, nextV2 := range valves {
+						if !opened[j] && i != j {
+							if exists && bestFlow > flow {
+								continue
+							} else {
+								nextTime1 := st.time1 - getPath(st.v1, nextV1) - 1
+								nextTime2 := st.time2 - getPath(st.v2, nextV2) - 1
+								if nextTime1 >= 0 || nextTime2 >= 0 {
+									nextFlow := flow
+									nextOpened := make([]bool, len(opened))
+									copy(nextOpened, opened)
+
+									if nextTime1 >= 0 {
+										nextFlow += nextTime1 * rts[nextV1]
+										nextOpened[i] = true
+									}
+
+									if nextTime2 >= 0 {
+										nextFlow += nextTime2 * rts[nextV2]
+										nextOpened[j] = true
+									}
+
+									if nextTime1 >= 0 && nextTime2 >= 0 {
+										rec(pathEnd{nextV1, nextV2, nextTime1, nextTime2}, nextFlow, nextOpened)
+									} else if nextTime1 >= 0 {
+										rec(pathEnd{nextV1, st.v2, nextTime1, st.time2}, nextFlow, nextOpened)
+									} else if nextTime2 >= 0 {
+										rec(pathEnd{st.v1, nextV2, st.time1, nextTime2}, nextFlow, nextOpened)
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		if flow > maxFlow {
+			maxFlow = flow
+		}
+	}
+
+	rec(pathEnd{0, 0, 26, 26}, 0, make([]bool, len(valves)))
 	return maxFlow
 }
 
@@ -56,14 +117,36 @@ func nonEmptyValves(rts rates) []valve {
 	return vs
 }
 
-func shortestPath(v1 valve, v2 valve, eds *edges) int {
+func shortestPathMemoized(eds edges) func(valve, valve) int {
+	paths := make(map[valve]map[valve]int)
+
+	return func(v1 valve, v2 valve) int {
+		if _, exists := paths[v1]; !exists {
+			paths[v1] = make(map[valve]int)
+		}
+		if _, exists := paths[v2]; !exists {
+			paths[v2] = make(map[valve]int)
+		}
+		if _, exists := paths[v1][v2]; !exists {
+			paths[v1][v2] = shortestPath(v1, v2, eds)
+			paths[v2][v1] = paths[v1][v2]
+		}
+		if res, ok := paths[v1][v2]; ok {
+			return res
+		} else {
+			panic("Path not found")
+		}
+	}
+}
+
+func shortestPath(v1 valve, v2 valve, eds edges) int {
 	d := make(map[valve]int)
-	for v := range *eds {
+	for v := range eds {
 		d[v] = math.MaxInt / 2
 	}
 	d[v1] = 0
-	for i := 1; i < len(*eds); i++ {
-		for v, neighbours := range *eds {
+	for i := 1; i < len(eds); i++ {
+		for v, neighbours := range eds {
 			for _, n := range neighbours {
 				if d[n] > d[v]+1 {
 					d[n] = d[v] + 1
--- day16/valves_test.go
+++ day16/valves_test.go
@@ -1,9 +1,22 @@
 package day16
 
-import "testing"
+import (
+	"testing"
 
-func TestXxx(t *testing.T) {
+	"github.com/stretchr/testify/require"
+)
+
+func TestCountMaxRate(t *testing.T) {
 	eds, rts := parseValves("valves.txt")
-	res := countMaxRate(eds, rts)
-	t.Log(res)
+	require.Equal(t, 2114, countMaxRate(eds, rts))
+}
+
+func TestCountMaxRate2Sample(t *testing.T) {
+	eds, rts := parseValves("valves_sample.txt")
+	require.Equal(t, 1707, countMaxRate2(eds, rts))
+}
+
+func TestCountMaxRate2(t *testing.T) {
+	eds, rts := parseValves("valves.txt")
+	require.Equal(t, 2666, countMaxRate2(eds, rts))
 }
